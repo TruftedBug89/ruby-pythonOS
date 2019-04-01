6d9c46dc6d1edecf5243b2eb849c7b07d76d9bc7
TruftedBug89 <39797722+TruftedBug89@users.noreply.github.com>
2019-04-01 20:11:17 +0200

    Update directory.py

--- TeGui/py/directory.py
+++ TeGui/py/directory.py
@@ -1,5 +1,6 @@
 #This class manages the directory data
 class directory(object):#
+	abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	#To start with the class its init function puts into self.data all the data from the txt
 	#Then puts on self.main all the folder names
 	def __init__(self):
@@ -16,6 +17,12 @@ class directory(object):#
 #This function creates a folder givin as parameter the folder name as string
 #Gives error if two folders are with the same name
 	def mkdir(self,newfolder):
+		for e in newfolder:
+			if e not in directory.abc:
+				outputwrite = open("./output/output.txt","w+")
+				outputwrite.write("SyntaxError: Use only alphabet letters")
+				outputwrite.close()
+				return
 		self.data = self.data.replace("\'","")
 		while len(newfolder) < 8:
 			newfolder += "-"
@@ -48,10 +55,46 @@ class directory(object):#
 		else:
 			outputwrite.write("Folder doesn't exist")
 		outputwrite.close()
-	# def rn(self,folder,newname):
-	# 	nameind = self.data.index(folder)
-	# 	self.data[nameind:nameind+8] = newname
-	# 	print(data)
+	def rn(self,oldname,newname):
+		datawrite = open("./output/directorydata.txt","w+")
+		outputwrite = open("./output/output.txt","w+")
+		notoriginal = oldname
+		while len(notoriginal) < 8:
+			notoriginal += "-"
+		notoriginal += ">"
+		if notoriginal in self.main:
+			outputwrite.write("Folder "+oldname+" succefully changed for "+newname)
+			while len(newname) < 8:
+				newname += "-"
+			newname += ">"
+			self.data.replace(notoriginal,newname)
+			datawrite.write(self.data)
+			datawrite.close()
+			outputwrite.close()
+			return
+		elif notoriginal not in self.main:
+			outputwrite.write("Error: No folder named :"+oldname)
+			datawrite.close()
+			outputwrite.close()
+			return
+		# elif oldname in sel.main:
+		# 	self.data
+		#
+		#
+		#
+		datawrite.close()
+		outputwrite.close()
+		# if oldname in self.main:
+		#
+		# nameind = self.data.index(folder)
+		# self.data[nameind:nameind+8] = newname
+		# print(data)
+
+
+
+
+
+
 
 inp = open("./output/input.txt","r+")
 inpread = str(inp.read())
@@ -61,6 +104,7 @@ for e in inpread:
 	if e == " ":
 		function = inpread[0:pos]
 		param = inpread[pos:]
+		break
 	pos += 1
 exe = directory()
 eval("exe."+function+"("+param+")")
