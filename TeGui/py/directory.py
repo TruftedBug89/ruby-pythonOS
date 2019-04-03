a79246346f99d0490bb63383301b7c78a6539f2f
TruftedBug89 <39797722+TruftedBug89@users.noreply.github.com>
2019-04-03 16:26:30 +0200

    Update directory.py

--- TeGui/py/directory.py
+++ TeGui/py/directory.py
@@ -1,10 +1,11 @@
+
 #This class manages the directory data
-class directory(object):#
+class directory(object):
 	abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	#To start with the class its init function puts into self.data all the data from the txt
 	#Then puts on self.main all the folder names
 	def __init__(self):
-		dataread = open("./output/directorydata.txt","r+")
+		dataread = open("../output/directorydata.txt","r+")
 		self.data = str(dataread.read())
 		dataread.close()
 		self.main = []
@@ -12,14 +13,18 @@ class directory(object):#
 		for e in self.data:
 			if e == ">":
 				self.main.append(self.data[count-8:count].replace("-",""))
-
 			count += 1
 #This function creates a folder givin as parameter the folder name as string
 #Gives error if two folders are with the same name
 	def mkdir(self,newfolder):
+		if len(newfolder) > 8:
+			outputwrite = open("../output/output.txt","w+")
+			outputwrite.write("SyntaxError: Name is too long, max. 8 characters !")
+			outputwrite.close()
+			return
 		for e in newfolder:
 			if e not in directory.abc:
-				outputwrite = open("./output/output.txt","w+")
+				outputwrite = open("../output/output.txt","w+")
 				outputwrite.write("SyntaxError: Use only alphabet letters")
 				outputwrite.close()
 				return
@@ -27,8 +32,8 @@ class directory(object):#
 		while len(newfolder) < 8:
 			newfolder += "-"
 		newfolder += ">"
-		datawrite = open("./output/directorydata.txt","w+")
-		outputwrite = open("./output/output.txt","w+")
+		datawrite = open("../output/directorydata.txt","w+")
+		outputwrite = open("../output/output.txt","w+")
 		if newfolder in self.data:
 			outputwrite.write("SyntaxError: Cant repeat folder name")
 			datawrite.write(self.data)
@@ -40,57 +45,50 @@ class directory(object):#
 #This function makes a ls on any folders if folder parameter is None or does a ls on
 #Specified folder if empty prints empty
 	def ls(self,folder):
-		outputwrite = open("./output/output.txt","w+")
+		outputwrite = open("../output/output.txt","w+")
 		if folder == None:
 			outputwrite.write(str(self.main).replace("\'",""))
 		elif folder in self.main:
 			inddata = self.data.index(folder)
-			indmain = self.main.index(folder)
-			indnext = self.data.index(self.main[indmain+1])
-			finalls = self.data[inddata:indnext]
-			print(finalls[len(finalls)-1])
-			if finalls[len(finalls)-1] == ">":
-				finalls += "Empty Folder !!"#Arreglar
-			outputwrite.write(finalls)
+			try:
+				indnext = self.data.index(self.main[self.main.index(folder)+1])
+			except IndexError:
+
+				indnext = len(self.data)-1
+			final = self.data[inddata:indnext]
+			if final[len(final)-1] == ">":
+				final += "Empty Folder !!"
+			outputwrite.write(final)
 		else:
 			outputwrite.write("Folder doesn't exist")
 		outputwrite.close()
 	def rn(self,oldname,newname):
-		datawrite = open("./output/directorydata.txt","w+")
-		outputwrite = open("./output/output.txt","w+")
-		notoriginal = oldname
-		while len(notoriginal) < 8:
-			notoriginal += "-"
-		notoriginal += ">"
-		if notoriginal in self.main:
+		if len(newname) > 8:
+			outputwrite = open("../output/output.txt","w+")
+			outputwrite.write("SyntaxError: Rename is too long, max. 8 characters !")
+			outputwrite.close()
+			return
+		outputwrite = open("../output/output.txt","w+")
+		original = oldname
+		if original in self.main:
+			datawrite = open("../output/directorydata.txt","w+")
 			outputwrite.write("Folder "+oldname+" succefully changed for "+newname)
+			while len(oldname) < 8:
+				oldname += "-"
+			oldname += ">"
 			while len(newname) < 8:
 				newname += "-"
 			newname += ">"
-			self.data.replace(notoriginal,newname)
+			self.data = self.data.replace(oldname,newname)
 			datawrite.write(self.data)
 			datawrite.close()
 			outputwrite.close()
 			return
-		elif notoriginal not in self.main:
+		elif original not in self.main:
 			outputwrite.write("Error: No folder named :"+oldname)
 			datawrite.close()
 			outputwrite.close()
 			return
-		# elif oldname in sel.main:
-		# 	self.data
-		#
-		#
-		#
-		datawrite.close()
-		outputwrite.close()
-		# if oldname in self.main:
-		#
-		# nameind = self.data.index(folder)
-		# self.data[nameind:nameind+8] = newname
-		# print(data)
-		
-		
 	def rm(self,foldername):
 		datawrite = open("./output/directorydata.txt","w+")
 		outputwrite = open("./output/output.txt","w+")
@@ -110,8 +108,9 @@ class directory(object):#
 
 
 
+functions = ["rn","ls","mkdir"]
 
-inp = open("./output/input.txt","r+")
+inp = open("../output/input.txt","r+")
 inpread = str(inp.read())
 inp.close()
 pos = 0
@@ -121,5 +120,8 @@ for e in inpread:
 		param = inpread[pos:]
 		break
 	pos += 1
-exe = directory()
-eval("exe."+function+"("+param+")")
+if function not in functions:
+	pass
+else:
+	exe = directory()
+	eval("exe."+function+"("+param+")")
